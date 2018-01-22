08b3cfda14b219e839d74146e8d5c57003a421ed
Tod Jackson <jtjacks@emory.edu>
2018-01-22 09:34:52 -0600

    added bitbucket variables to be replaced by pipeline

--- emory-vpcprovisioning-webapp/src/edu/emory/oit/vpcprovisioning/shared/ReleaseInfo.java
+++ emory-vpcprovisioning-webapp/src/edu/emory/oit/vpcprovisioning/shared/ReleaseInfo.java
@@ -10,6 +10,16 @@ public class ReleaseInfo implements IsSerializable {
 	String version = "1.0";
 	String build = "20";
 	String productName = "VPC Provisioning App";
+	String applicationEnvironment = "APPLICATION_ENVIRONMENT";
+	String applicationName = "APPLICATION_NAME";
+	String awsDefaultRegion = "AWS_DEFAULT_REGION";
+	String gwtProjectName = "GWT_PROJECT_NAME";
+	String mvnArtifactId = "MVN_ARTIFACT_ID";
+	String mvnPackaging = "MVN_PACKAGING";
+	String mvnVersion = "MVN_VERSION";
+	String s3Bucket = "S3_BUCKET";
+	String branchName = "BRANCH_NAME";
+	String buildNumber = "BUILD_NUMBER";
 	
 	public String getProductName() {
 		return productName;
@@ -41,5 +51,85 @@ public class ReleaseInfo implements IsSerializable {
 	public String toString() {
 		return 
 				"Version: " + version + "  Build: " + build;
+	}
+
+	public String getApplicationEnvironment() {
+		return applicationEnvironment;
+	}
+
+	public void setApplicationEnvironment(String applicationEnvironment) {
+		this.applicationEnvironment = applicationEnvironment;
+	}
+
+	public String getApplicationName() {
+		return applicationName;
+	}
+
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}
+
+	public String getAwsDefaultRegion() {
+		return awsDefaultRegion;
+	}
+
+	public void setAwsDefaultRegion(String awsDefaultRegion) {
+		this.awsDefaultRegion = awsDefaultRegion;
+	}
+
+	public String getGwtProjectName() {
+		return gwtProjectName;
+	}
+
+	public void setGwtProjectName(String gwtProjectName) {
+		this.gwtProjectName = gwtProjectName;
+	}
+
+	public String getMvnArtifactId() {
+		return mvnArtifactId;
+	}
+
+	public void setMvnArtifactId(String mvnArtifactId) {
+		this.mvnArtifactId = mvnArtifactId;
+	}
+
+	public String getMvnPackaging() {
+		return mvnPackaging;
+	}
+
+	public void setMvnPackaging(String mvnPackaging) {
+		this.mvnPackaging = mvnPackaging;
+	}
+
+	public String getMvnVersion() {
+		return mvnVersion;
+	}
+
+	public void setMvnVersion(String mvnVersion) {
+		this.mvnVersion = mvnVersion;
+	}
+
+	public String getS3Bucket() {
+		return s3Bucket;
+	}
+
+	public void setS3Bucket(String s3Bucket) {
+		this.s3Bucket = s3Bucket;
+	}
+
+	public String getBranchName() {
+		return branchName;
+	}
+
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+
+	public String getBuildNumber() {
+		return buildNumber;
+	}
+
+	public void setBuildNumber(String buildNumber) {
+		this.buildNumber = buildNumber;
 	}
 }
